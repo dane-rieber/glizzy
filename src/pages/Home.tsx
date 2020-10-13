8c389c8f990931b97c44a3eabddc0ea650efb70e
Cole Radetich <colejradetich@gmail.com>
2020-10-13 17:35:00 -0600

    Buttons Added
    woop woop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import MessageListItem from '../components/MessageListItem';
 import React, { useState } from 'react';
+
 import { Message, getMessages } from '../data/messages';
 import {
+  IonButton,
+  IonToggle,
   IonContent,
   IonHeader,
   IonInput,
@@ -34,6 +37,10 @@ const Home: React.FC = () => {
     }, 3000);
   };
 
+
+  //buttons:
+
+  //add features for sort buttons and light/dark mode
   return (
     <IonPage id="home-page">
       <IonHeader>
@@ -43,13 +50,26 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
       <IonInput value={searchText} placeholder="Search for Items" onIonChange={e => setSearchText(e.detail.value!)}></IonInput>
+
+
+      <IonButton color="secondary" href="#">Sort by Name</IonButton>
+      <IonButton color="secondary" href="#">Sort by Store</IonButton>
+      <IonButton color="secondary" href="#">Sort by Price</IonButton>
+
         <IonList>
           {groceries.filter(g =>
             searchText === undefined ||
             g.name.toLowerCase().includes(searchText.toLowerCase()) ||
-            g.store.toLowerCase().includes(searchText.toLowerCase()))
+            g.store.toLowerCase().includes(searchText.toLowerCase())).sort((a, b) => a.name.localeCompare(b.name))
             .map(g => <GroceryListItem key={g.id} grocery={g} />)}
         </IonList>
+        
+        <IonButton color="primary" expand="full" href="#">Add Item</IonButton>
+
+
+        <IonButton color="tertiary" href="#">Settings</IonButton>
+        <IonButton color="light" shape="round" href="#">Light/Dark Mode Toggle</IonButton>
+
       </IonContent>
     </IonPage>
   );
