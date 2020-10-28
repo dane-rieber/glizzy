9205ed9262a3c45f6210ec6d159dc7f7cf18b22c
Dane Rieber <dane.rieber@colorado.edu>
2020-10-28 17:44:59 -0600

    Added patch endpoints to update groceries/lists, modified get /list endpoint

--- server/server.js
+++ server/server.js
@@ -104,9 +104,47 @@ app.get('/list/:id', (req, res) => {
                 res.status(401).json({ status: 'Unauthorized', message: 'List does not belong to user' });
                 return;
             }
+            
+            db.task(t => {
+                return t.batch([
+                    t.one(`select * from list where id = ${list_id};`),
+                    t.any(`select * from grocery where list_id = ${list_id};`)
+                ])
+            })
+                .then(results => res.status(200).json({list: results[0], groceries: results[1]}))
+                .catch(err => sendError(err, res))
+        })
+        .catch(err => sendError(err, res))
+})
 
-            db.any(`select * from grocery where list_id = ${list_id};`)
-                .then(groceries => res.status(200).json(groceries)) // return grocery list with provided id
+app.patch('/list/:id', (req, res) => {
+    var token = get_token(req);
+    if (!token) {
+        res.status(401).json({ status: 'Unauthorized', message: 'Auth token is invalid' });
+        return;
+    }
+
+    var list_id = req.params.id;
+    if (!list_id) {
+        res.status(400).json({ message: 'Please specify a list id as a url parameter' });
+        return;
+    }
+
+    var name = req.body.name;
+    if (!name) {
+        res.status(400).json({ message: 'New list name must be provided in request body' });
+        return;
+    }
+
+    db.one(`select user_id from list where id = ${list_id};`)
+        .then(list => {
+            if (list.user_id !== token.user_id) {
+                res.status(401).json({ status: 'Unauthorized', message: 'List does not belong to user' });
+                return;
+            }
+
+            db.one(`update list set name = '${name}' where id = ${list_id} returning id;`)
+                .then(id => res.status(200).json({ message: 'List updated', list_id: id })) // returns id of updated list
                 .catch(err => sendError(err, res))
         })
         .catch(err => sendError(err, res))
@@ -150,7 +188,7 @@ app.post('/list/:id/add', (req, res) => {
                 queryFields += 'name';
                 queryValues += `'${name}'`;
             } else {
-                res.status(400).json({message: 'Grocery name must be provided in request body'})
+                res.status(400).json({ message: 'Grocery name must be provided in request body' })
                 return;
             }
 
@@ -170,12 +208,12 @@ app.post('/list/:id/add', (req, res) => {
                 queryFields += ',active';
                 queryValues += `,${active}`;
             }
-            
+
             queryFields += ',list_id';
             queryValues += `,${list_id}`;
 
             db.one(`insert into grocery(${queryFields}) values (${queryValues}) returning id;`)
-                .then(id => res.status(200).json({message: 'Grocery created', grocery_id: id})) // Returns id of created grocery
+                .then(id => res.status(200).json({ message: 'Grocery created', grocery_id: id })) // Returns id of created grocery
                 .catch(err => sendError(err, res))
         })
         .catch(err => sendError(err, res))
@@ -215,6 +253,63 @@ app.get('/grocery/:id', (req, res) => {
         .catch(err => sendError(err, res))
 })
 
+app.patch('/grocery/:id', (req, res) => {
+    var token = get_token(req);
+    if (!token) {
+        res.status(401).json({ status: 'Unauthorized', message: 'Auth token is invalid' });
+        return;
+    }
+
+    var grocery_id = req.params.id;
+    if (!grocery_id) {
+        res.status(400).json({ message: 'Please specify a grocery id as a url parameter' });
+        return;
+    }
+
+    // Check that the user accessing the grocery owns the list that contains the grocery
+    db.one(`select user_id from list where id = (select list_id from grocery where id = ${grocery_id});`)
+        .then(list => {
+            if (list.user_id !== token.user_id) {
+                res.status(401).json({ status: 'Unauthorized', message: 'Grocery does not belong to user' });
+                return;
+            }
+
+            /*
+            The following code builds a psql query depending on which parameters are specified in the request.
+            This way, not all paramaters need to be specified when updating a grocery object.
+            */
+
+            var name = req.body.name;
+            var quantity = req.body.quantity;
+            var store_id = req.body.store_id;
+            var price = req.body.price;
+            var active = req.body.active;
+
+            var query = '';
+
+            if (name) query += `name='${name}',`;
+            if (quantity) query += `quantity=${quantity},`;
+            if (store_id) query += `store_id=${store_id},`;
+            if (price) query += `price='${price}',`;
+            if (active) query += `active=${active}`;
+
+            if (query === '') {
+                res.status(400).json({ message: 'Request body did not contain information to update' });
+                return;
+            }
+
+            // Remove trailing commas from query string
+            if (query[query.length - 1] === ',') {
+                query = query.substr(0, query.length - 1);
+            }
+
+            db.one(`update grocery set ${query} where id = ${grocery_id} returning id;`)
+                .then(id => res.status(200).json({ message: 'Grocery item updated', grocery_id: id })) // return id of updated grocery
+                .catch(err => sendError(err, res))
+        })
+        .catch(err => sendError(err, res))
+})
+
 /*
 
 STORE ENDPOINTS
@@ -239,7 +334,7 @@ app.post('/create/user', (req, res) => {
     var password = req.body.password;
 
     if (!username || !password) {
-        res.status(400).json({message: 'New username/password must be provided in request body'});
+        res.status(400).json({ message: 'New username/password must be provided in request body' });
         return;
     }
 
@@ -247,12 +342,12 @@ app.post('/create/user', (req, res) => {
     db.any(`select id from glizzy_user where username = '${username}';`)
         .then(rows => {
             if (rows.length !== 0) {
-                res.status(401).json({message: 'User already exists'})
+                res.status(401).json({ message: 'User already exists' })
                 return;
             }
-            
+
             db.none(`insert into glizzy_user(username, password) values ('${username}', '${password}');`)
-                .then(() => res.status(200).json({message: 'User created', username: username})) // Returns username of newly created user
+                .then(() => res.status(200).json({ message: 'User created', username: username })) // Returns username of newly created user
                 .catch(err => sendError(err, res))
         })
         .catch(err => sendError(err, res))
@@ -267,12 +362,12 @@ app.post('/create/list', (req, res) => {
 
     var name = req.body.name;
     if (!name) {
-        res.status(400).json({message: 'List name must be provided in request body'});
+        res.status(400).json({ message: 'List name must be provided in request body' });
         return;
     }
 
     db.one(`insert into list(name, user_id) values ('${name}', ${token.user_id}) returning id;`)
-        .then(id => res.status(200).json({message: 'List created', list_id: id})) // returns id of newly created list
+        .then(id => res.status(200).json({ message: 'List created', list_id: id })) // returns id of newly created list
         .catch(err => sendError(err, res))
 })
 
